7e93957d0f9038354b52b76d472a50ed120386d3
Shery11 <shery_kirmani@hotmail.com>
2017-07-27 15:56:40 +0500

    new pages added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,46 +7,10 @@ import { trigger,state,style,transition,animate,keyframes } from '@angular/anima
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations:[
-     trigger('myAnimation',[
-           state('small',style({
-           	transform: 'scale(1)',
-           })),
-           state('large',style({
-           	transform: 'scale(1.2)',
-           })),
-
-           transition('small <=> large',animate('300ms ease-in')),
-     	]),
-  ]
+  
 })
 export class AppComponent {
 
+  title = "My application";
 
-	state:string ="small"
-
-	animateMe(){
-		this.state = (this.state === 'small' ? 'large' : 'small');
-	}
-   
-   title = 'app';
-
-   myArr = ['i','we','you','they'];
-
-    constructor(private dataService:DataService){
-
-    }
-    
-     property:String ="";
-
-     ngOnInit(){
-     	console.log(this.dataService.cars);
-     	this.property = this.dataService.myData();
-     }
-
-    myEvent(event){
-
-   	console.log("clicked");
-
-   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
-import { DataService } from './data.service';
 import { BrowserAnimationsModule }  from '@angular/platform-browser/animations';
+import { AdminComponent } from './admin/admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LandingComponent } from './landing/landing.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { EventComponent } from './event/event.component';
+import { StartupComponent } from './startup/startup.component';
+import { AboutComponent } from './about/about.component';
+import { DashboardHeaderComponent } from './dashboard-header/dashboard-header.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 
@@ -12,14 +25,83 @@ import { BrowserAnimationsModule }  from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-    MyNewComponentComponent
+    AdminComponent,
+    DashboardComponent,
+    HomeComponent,
+    ProfileComponent,
+    LandingComponent,
+    HeaderComponent,
+    SidebarComponent,
+    PageNotFoundComponent,
+    EventComponent,
+    StartupComponent,
+    AboutComponent,
+    DashboardHeaderComponent,
+    LoginComponent,
+    RegisterComponent,
+   
+   
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+       {
+          path:'',
+          redirectTo:'landing',
+          pathMatch: 'full'
+       },
+       {
+         path:'landing',
+         component: LandingComponent
+       },
+       {
+            path:'home',
+            component: HomeComponent
+       },
+       {
+           path : 'dashboard',
+           component: DashboardComponent,
+           children : [
+
+            {
+                path:'',
+                redirectTo:'home',
+                pathMatch: 'full'
+            },
+            {
+               path: 'home',
+               component : HomeComponent    
+            },           
+            {
+              path : 'profile',
+              component : ProfileComponent
+            },
+            {
+              path: 'events',
+              component: EventComponent
+            },
+            {
+              path : 'startups',
+              component : StartupComponent
+            },
+            {
+              path: 'about',
+              component : AboutComponent
+            }
+           ]
+       },
+       {
+         path: 'profile',
+         component:ProfileComponent
+       },
+       { path: '**', 
+         component: PageNotFoundComponent 
+       }
+    ])
 
   ],
-  providers: [DataService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
